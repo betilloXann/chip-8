66f3d89b5ccfe22085b62d93ffc87be5978c74fc
Alberto Hernandez <alberto10pumas@gmail.com>
2026-04-25 22:58:37 -0600

    :feat: Se agrego fetch

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,1 +1,17 @@
 //Lógica del CPU (fetch, decode, execute)
+impl Chip8 {
+    fn fetch(&mut self) -> u16 {
+        let firts = self.memory[self.pc as usize]; // 170
+        let second = self.memory[(self.pc + 1) as usize]; // 187
+
+        let opcode = ((first as u16) << 8) | (second as u16);
+
+        self.pc += 2;
+
+        opcode
+    }
+
+    fn decode() {}
+
+    fn execute() {}
+}

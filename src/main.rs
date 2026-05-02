204332f30446ec5a9080526e5ad50d4a8f45f04a
Alberto Hernandez <alberto10pumas@gmail.com>
2026-05-01 18:06:30 -0600

    feat: soporte para entrada de teclado (keypad)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,41 @@ use std::time::Duration;
 mod chip8;
 mod cpu;
 
+fn actualizar_teclado(window: &Window, chip8: &mut Chip8) {
+    //Teclado limpio
+    chip8.keypad.fill(false);
+
+    //Obtenemos teclas presionadas actualmente
+    let keys = window.get_keys();
+
+    //Mapeado de tecla
+    for key in keys {
+        match key {
+            Key::Key1 => chip8.keypad[0x1] = true,
+            Key::Key2 => chip8.keypad[0x2] = true,
+            Key::Key3 => chip8.keypad[0x3] = true,
+            Key::Key4 => chip8.keypad[0xC] = true,
+
+            Key::Q => chip8.keypad[0x4] = true,
+            Key::W => chip8.keypad[0x5] = true,
+            Key::E => chip8.keypad[0x6] = true,
+            Key::R => chip8.keypad[0xD] = true,
+
+            Key::A => chip8.keypad[0x7] = true,
+            Key::S => chip8.keypad[0x8] = true,
+            Key::D => chip8.keypad[0x9] = true,
+            Key::F => chip8.keypad[0xE] = true,
+
+            Key::Z => chip8.keypad[0xA] = true,
+            Key::X => chip8.keypad[0x0] = true,
+            Key::C => chip8.keypad[0xB] = true,
+            Key::V => chip8.keypad[0xF] = true,
+
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     //Configuracion de Pantalla
     let mut window = Window::new(
@@ -23,12 +58,14 @@ fn main() {
     window.limit_update_rate(Some(Duration::from_micros(16667)));
 
     let mut chip8 = Chip8::new();
-    let rom = fs::read("src/roms/BETILLO.ch8").expect("No se puede leer la ROM");
+    let rom = fs::read("src/roms/4-flags.ch8").expect("No se puede leer la ROM");
     chip8.load_rom(&rom);
 
     let mut buffer: Vec<u32> = vec![0; 64 * 32];
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        //Teclado
+        actualizar_teclado(&window, &mut chip8);
         // Control de velocidad de la CPU
         for _ in 0..8 {
             chip8.cycle();
